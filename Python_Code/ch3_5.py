c23118832176f20f1fd6f9a143418ed9174ecfbf
Shoko <61862770+anemptyarchive@users.noreply.github.com>
2020-11-12 18:36:34 +0900

    update gif

--- Python_Code/ch3_5.py
+++ Python_Code/ch3_5.py
@@ -111,7 +111,7 @@ m_hat_m = np.dot(np.linalg.inv(lambda_hat_mm), tmp_m_m)
 
 #%%
 
-# 事前分布からwをサンプリング
+# 事後分布からwをサンプリング
 x_mline = x_vector(x_line, M)
 smp_model_arr = np.empty((smp_size, len(x_line)))
 for i in range(smp_size):
@@ -139,9 +139,9 @@ plt.show()
 # 予測分布のパラメータを計算
 sigma2_star_hat_line = np.repeat(1 / lmd, len(x_line))
 for i in range(len(x_line)):
-    sigma2_star_hat_line[i] += x_mline[:, 1].T.dot(np.linalg.inv(lambda_hat_mm)).dot(x_mline[:, i])
+    sigma2_star_hat_line[i] += x_mline[:, i].T.dot(np.linalg.inv(lambda_hat_mm)).dot(x_mline[:, i])
 mu_star_hat_line = np.dot(m_hat_m.T, x_mline).flatten()
-print(sigma2_star_hat_line)
+
 #%%
 
 # 予測分布を作図
@@ -158,4 +158,140 @@ plt.ylabel('y')
 plt.grid()
 plt.legend()
 plt.show()
+
+
+#%%
+
+# gif画像で推移を確認
+
+# 利用ライブラリ
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+# xのベクトル作成関数を定義
+def x_vector(x_smp_n, M):
+    x_mn = np.zeros(shape=(M, len(x_smp_n)))
+    for m in range(M):
+        x_mn[m, :] = np.power(x_smp_n, m)
+    return x_mn
+
+
+#%%
+
+# 観測モデルのパラメータを指定
+M_truth = 4
+sigma = 1.0
+lmd = 1.0 / sigma**2
+w_m = np.random.choice(
+    np.arange(-1.0, 1.0, step=0.1), size=M_truth, replace=True
+).reshape([M_truth, -1])
+
+# 作図用のx軸の値
+x_line = np.arange(-3.0, 3.0, step=0.01)
+y_line = np.dot(w_m.T, x_vector(x_line, M_truth)).flatten()
+
+# ノイズを含まない観測モデルを作図
+plt.plot(x_line, y_line)
+plt.title('Observation Model', fontsize=20)
+plt.xlabel('x')
+plt.ylabel('y')
+plt.grid()
+plt.show()
+
+#%%
+
+# データ数を指定(試行回数)
+N = 100
+
+# 事前分布のパラメータを指定
+M = 10
+m_m = np.zeros((M, 1))
+sigma_mm = np.identity(M)
+lambda_mm = np.linalg.inv(sigma_mm**2)
+
+# 作図用のx軸の値
+x_mline = x_vector(x_line, M)
+
+
+# 推論
+x_smp_n = np.array([])
+y_1n = np.array([[]])
+for n in range(N):
+    
+    # データをサンプリング
+    smp_x = np.random.choice(
+        np.arange(min(x_line), max(x_line), step=0.01), size=1, replace=True
+    )
+    x_smp_n = np.append(x_smp_n, smp_x)
+    
+    # 出力値を計算
+    tmp_y = np.dot(w_m.T, x_vector(x_smp_n[n:n+1], M_truth))
+    tmp_y += np.random.normal(loc=0.0, scale=1 / lmd, size=1) # ノイズ成分
+    y_1n = np.append(y_1n, tmp_y, axis=1)
+    
+    # xのベクトル作成
+    x_mn = x_vector(x_smp_n[n:n+1], M)
+    
+    # 事後分布のパラメータを更新
+    old_lambda_mm = lambda_mm.copy()
+    lambda_mm += lmd * np.dot(x_mn, x_mn.T)
+    tmp_m_m = lmd * np.dot(x_mn, y_1n[0:1, n:n+1].T)
+    tmp_m_m += np.dot(old_lambda_mm, m_m)
+    m_m = np.dot(np.linalg.inv(lambda_mm), tmp_m_m)
+    
+    # 予測分布のパラメータを計算
+    sigma2_star_line = np.repeat(1 / lmd, len(x_line))
+    for i in range(len(x_line)):
+        sigma2_star_line[i] += x_mline[:, i].T.dot(np.linalg.inv(lambda_mm)).dot(x_mline[:, i])
+    mu_star_line = np.dot(m_m.T, x_mline).flatten()
+    
+    # 推移を記録
+    if n == 0:
+        sigma2_star_arr = np.array([sigma2_star_line])
+        mu_star_arr = np.array([mu_star_line])
+    elif n > 0:
+        sigma2_star_arr = np.append(sigma2_star_arr, [sigma2_star_line.copy()], axis=0)
+        mu_star_arr = np.append(mu_star_arr, [mu_star_line.copy()], axis=0)
+
+#%%
+        
+# 追加ライブラリ
+import matplotlib.animation as animation
+
+
+# グラフを初期化
+plt.cla()
+
+# 予測分布を作図
+fig = plt.figure()
+ax = fig.add_subplot(1, 1, 1)
+img = []
+for n in range(N):
+    # nフレーム目のグラフを描画
+    ims_predit, = ax.plot(x_line, mu_star_arr[n], color='orange', label='predict') # 予測分布の期待値
+    ims_psigma, = ax.plot(x_line, mu_star_arr[n] + np.sqrt(sigma2_star_arr[n]), 
+                          color='#00A968', linestyle='--', label='$+\sigma$') # +sigma
+    ims_msigma, = ax.plot(x_line, mu_star_arr[n] - np.sqrt(sigma2_star_arr[n]), 
+                          color='#00A968', linestyle='--', label='$-\sigma$') # -sigma
+    ims_model, = ax.plot(x_line, y_line, linestyle=':', color='blue', label='model') # 観測モデル
+    ims_x = ax.scatter(x_smp_n[0:n+1], y_1n[0, 0:n+1].flatten(), color='chocolate') # 観測データ
+    
+    # グラフの設定
+    ax.set_xlabel('x')
+    ax.set_ylabel('y')
+    ax.set_ylim(min(y_line), max(y_line))
+    ax.grid(True)
+    
+    # グラフを格納
+    img.append([ims_predit, ims_psigma, ims_msigma, ims_model, ims_x])
+
+# gif画像を作成
+ani = animation.ArtistAnimation(fig, img, interval=100)
+ani.save("test.gif")
+
+
+#%%
+
+print('end')
 
