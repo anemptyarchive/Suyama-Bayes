b07595870bfa4a76d7f2c69c66c497abaf508eff
Shoko <61862770+anemptyarchive@users.noreply.github.com>
2021-02-22 11:50:17 +0900

    upload ch3.2.1 R fig

--- R_Code/ch3_2_2.R
+++ R_Code/ch3_2_2.R
@@ -13,13 +13,13 @@ K <- 3
 # 真のパラメータを指定
 pi_truth_k <- c(0.3, 0.5, 0.2)
 
-# 観測モデル(カテゴリ分布)のデータフレームを作成
+# 尤度(カテゴリ分布)のデータフレームを作成
 model_df <- tibble(
   k = 1:K, # 次元番号
   prob = pi_truth_k # 確率
 )
 
-# 観測モデルを作図
+# 尤度を作図
 ggplot(model_df, aes(x = k, y = prob)) + 
   geom_bar(stat = "identity", position = "dodge", fill = "purple") + # 観測モデル
   ylim(c(0, 1)) + # y軸の表示範囲
@@ -63,15 +63,16 @@ pi_point <- tibble(
 pi_point <- pi_point / rowSums(pi_point) # 正規化
 
 # 点を間引く(ハイスぺ機なら不要…)
-pi_point <- round(pi_point, 3) %>% # 値を丸め込み
+pi_point <- pi_point[-1, ] %>%  # (0, 0, 0)の行を除去
+  round(3) %>% # 値を丸め込み
   dplyr::as_tibble() %>% # データフレームに変換
   dplyr::distinct(pi_1, pi_2, pi_3) %>% # 重複を除去
   as.matrix() # マトリクスに再変換
 
 # 作図用のpiの値をランダムに生成
 #pi_point <- seq(0, 1, 0.001) %>% # piがとり得る値
-#  sample(size = 90000, replace = TRUE) %>% 
-#  matrix(ncol = 3)
+#  sample(size = 10000 * K, replace = TRUE) %>% # ランダムに値を生成
+#  matrix(ncol = K) # マトリクスに変換
 #pi_point <- pi_point / rowSums(pi_point) # 正規化
 
 # 事後分布(ディリクレ分布)を計算
@@ -89,19 +90,18 @@ ggplot() +
   scale_x_continuous(breaks = c(0, 1), 
                      labels = c("(1, 0, 0)", "(0, 1, 0)")) + # x軸目盛
   scale_y_continuous(breaks = c(0, 0.87), 
-                     labels = c("(1, 0, 0)", "(0, 1, 0)")) + # y軸目盛
+                     labels = c("(1, 0, 0)", "(0, 0, 1)")) + # y軸目盛
   coord_fixed(ratio = 1) + # 縦横比
   labs(title = "Dirichlet Distribution", 
-       subtitle = paste0("N=", N, ", alpha=(", paste(alpha_k, collapse = ", "), ")"), 
+       subtitle = paste0("alpha=(", paste(alpha_k, collapse = ", "), ")"), 
        x = expression(paste(pi[1], ", ", pi[2], sep = "")), 
        y = expression(paste(pi[1], ", ", pi[3], sep = "")))
 
 
 ### 事後分布の計算 -----
 
-# 事後分布のパラメータを計算
+# 事後分布のパラメータを計算:式(3.28)
 alpha_hat_k <- colSums(s_nk) + alpha_k
-
 
 # 事後分布(ディリクレ分布)を計算
 posterior_df <- tibble(
@@ -125,7 +125,7 @@ ggplot() +
   scale_x_continuous(breaks = c(0, 1), 
                      labels = c("(1, 0, 0)", "(0, 1, 0)")) + # x軸目盛
   scale_y_continuous(breaks = c(0, 0.87), 
-                     labels = c("(1, 0, 0)", "(0, 1, 0)")) + # y軸目盛
+                     labels = c("(1, 0, 0)", "(0, 0, 1)")) + # y軸目盛
   coord_fixed(ratio = 1) + # 縦横比
   labs(title = "Dirichlet Distribution", 
        subtitle = paste0("N=", N, ", alpha_hat=(", paste(alpha_hat_k, collapse = ", "), ")"), 
@@ -177,18 +177,18 @@ alpha_k <- c(1, 1, 1)
 # 作図用のpiの値を満遍なく生成
 point_vec <- seq(0, 1, by = 0.025) # piがとり得る値
 n_point <- length(point_vec) # 点の数
-pi_point <- tibble(
+pi_point_df <- tibble(
   pi_1 = rep(rep(point_vec, times = n_point), times = n_point), 
   pi_2 = rep(rep(point_vec, each = n_point), times = n_point), 
   pi_3 = rep(point_vec, each = n_point^2)
 )
-pi_point <- pi_point / rowSums(pi_point) # 正規化
+pi_point_df <- pi_point_df / rowSums(pi_point_df) # 正規化
 
 # 点を間引く(ハイスぺ機なら不要…)
-pi_point <- round(pi_point, 3) %>% # 値を丸め込み
-  dplyr::as_tibble() %>% # データフレームに変換
+pi_point <- pi_point_df[-1, ] %>%  # (0, 0, 0)の行を除去
+  round(3) %>% # 値を丸め込み
   dplyr::distinct(pi_1, pi_2, pi_3) %>% # 重複を除去
-  as.matrix() # マトリクスに再変換
+  as.matrix() # マトリクスに変換
 
 
 # 事前分布(ディリクレ分布)を計算
@@ -207,7 +207,7 @@ pi_star_k <- alpha_k / sum(alpha_k)
 predict_df <- tibble(
   k = 1:K, # 次元番号
   prob = pi_star_k, # 確率
-  label = as.factor(paste0("N=", 0, ", pi_star=(", paste0(pi_star_k, collapse = ", "), ")")) # 試行回数とパラメータのラベル
+  label = as.factor(paste0("N=", 0, ", pi_star=(", paste0(round(pi_star_k, 2), collapse = ", "), ")")) # 試行回数とパラメータのラベル
 )
 
 
@@ -271,7 +271,7 @@ posterior_graph <- ggplot() +
   scale_x_continuous(breaks = c(0, 1), 
                      labels = c("(1, 0, 0)", "(0, 1, 0)")) + # x軸目盛
   scale_y_continuous(breaks = c(0, 0.87), 
-                     labels = c("(1, 0, 0)", "(0, 1, 0)")) + # y軸目盛
+                     labels = c("(1, 0, 0)", "(0, 0, 1)")) + # y軸目盛
   coord_fixed(ratio = 1) + # 縦横比
   gganimate::transition_manual(label) + # フレーム
   labs(title = "Dirichlet Distribution", 
@@ -305,6 +305,7 @@ predict_graph <- ggplot() +
   geom_bar(data = model_df, aes(x = k, y = prob), stat = "identity", position = "dodge", 
            alpha = 0, color = "red", linetype = "dashed") + # 真の分布
   gganimate::transition_manual(label) + # フレーム
+  ylim(c(0, 1)) + # y軸の表示範囲
   labs(title = "Categorical Distribution", 
        subtitle = "{current_frame}")
 
