1f4016cf4345ae83a5f7695bd0c9b73d05bd272a
Shoko <61862770+anemptyarchive@users.noreply.github.com>
2021-02-28 22:46:59 +0900

    Update ch3_4_2.R

--- R_Code/ch3_4_2.R
+++ R_Code/ch3_4_2.R
@@ -1,254 +1,289 @@
 
-# ch 3.4.2 多次元ガウス分布：精度が未知の場合 ----------------------------------------------
+# 3.4.2 多次元ガウス分布：精度が未知の場合 ----------------------------------------------
 
-# 事後分布 --------------------------------------------------------------------
-
-# 利用パッケージ
+# 3.4.2項で利用するパッケージ
 library(tidyverse)
-library(mvtnorm)
 library(mvnfast)
 
 
-# データ数を指定
-N <- 50
-D <- 2 # (固定)
+### 尤度(多次元ガウス分布)の設定 -----
 
-# 観測モデルのパラメータを指定
+# 真のパラメータを指定
 mu_d <- c(25, 50)
-sigma_truth_dd <- matrix(c(50, 30, 30, 50), nrow = 2, ncol = 2)
+sigma_truth_dd <- matrix(c(20, 15, 15, 30), nrow = 2, ncol = 2)
 lambda_truth_dd <- solve(sigma_truth_dd^2)
 
-# 事前分布のパラメータを指定
-W_dd <- matrix(c(0.00005, 0, 0, 0.00005), nrow = 2, ncol = 2)
-nu <- 2
-
-
-# 作図用の点を生成
-x_vec <- seq(mu_d[1] - 2 * sigma_truth_dd[1, 1], mu_d[1] + 2 * sigma_truth_dd[1, 1], by = 0.5)
-y_vec <- seq(mu_d[2] - 2 * sigma_truth_dd[2, 2], mu_d[2] + 2 * sigma_truth_dd[2, 2], by = 0.5)
-point_df <- tibble(
-  x = rep(x_vec, times = length(y_vec)), 
-  y = rep(y_vec, each = length(x_vec))
-)
-mu_df <- tibble(
-  x = mu_d[1], 
-  y = mu_d[2]
+# 作図用のxの点を作成
+x_1_vec <- seq(mu_d[1] - 4 * sigma_truth_dd[1, 1], mu_d[1] + 4 * sigma_truth_dd[1, 1], by = 0.25)
+x_2_vec <- seq(mu_d[2] - 4 * sigma_truth_dd[2, 2], mu_d[2] + 4 * sigma_truth_dd[2, 2], by = 0.25)
+x_point_mat <- cbind(
+  rep(x_1_vec, times = length(x_2_vec)), 
+  rep(x_2_vec, each = length(x_1_vec))
 )
 
+# 尤度を計算:式(2.72)
+model_df <- tibble(
+  x_1 = x_point_mat[, 1], 
+  x_2 = x_point_mat[, 2], 
+  density = mvnfast::dmvn(
+    X = x_point_mat, mu = mu_d, sigma = solve(lambda_truth_dd)
+  )
+)
 
-# 2次元ガウス分布に従うデータを生成
-x_nd <- mvtnorm::rmvnorm(n = N, mean = mu_d, sigma = sigma_truth_dd^2)
+# 尤度を作図
+ggplot(model_df, aes(x = x_1, y = x_2, z = density, color = ..level..)) + 
+  geom_contour() + # 尤度
+  labs(title = "Multivariate Gaussian Distribution", 
+       subtitle = paste0("mu=(", paste(round(mu_d, 1), collapse = ", "), ")", 
+                         ", lambda=(", paste(round(lambda_truth_dd, 5), collapse = ", "), ")"), 
+       x = expression(x[1]), y = expression(x[2]), 
+       color = "density")
+
+
+### 観測データの生成 -----
+
+# (観測)データ数を指定
+N <- 100
+
+# 多次元ガウス分布に従うデータを生成
+x_nd <- mvnfast::rmvn(n = N, mu = mu_d, sigma = solve(lambda_truth_dd))
+#x_nd <- mvtnorm::rmvnorm(n = N, mean = mu_d, sigma = solve(lambda_truth_dd))
+
+# 観測データを確認
 summary(x_nd)
 
-# 観測データのデータフレーム
-sample_df <- tibble(
-  x = x_nd[, 1], 
-  y = x_nd[, 2]
+# 観測データのデータフレームを作成
+x_df <- tibble(
+  x_n1 = x_nd[, 1], 
+  x_n2 = x_nd[, 2]
 )
 
-# 観測モデルのデータフレーム
-model_df <- tibble(
-  xy = point_df, 
-  density = mvtnorm::dmvnorm(x = xy, mean = mu_d, sigma = sigma_truth_dd^2), # 確率密度
-) %>% 
-  dplyr::select(density) %>% 
-  cbind(point_df, .)
-
-# 観測データの散布図を作成
+# 観測データの散布図を作図
 ggplot() + 
-  geom_point(data = sample_df, aes(x = x, y = y)) + # 観測データ
-  geom_contour(data = model_df, aes(x, y, z = density, color = ..level..)) + # 観測モデル
-  geom_point(data = mu_df, aes(x = x, y = y), color = "red", shape = 3, size = 5) + # 平均パラメータ
+  geom_point(data = x_df, aes(x = x_n1, y = x_n2)) + # 観測データ
+  geom_contour(data = model_df, aes(x = x_1, y = x_2, z = density, color = ..level..)) + # 尤度
   labs(title = "Multivariate Gaussian Distribution", 
-       subtitle = paste0("N=", N, ", mu=(", paste(round(mu_d, 1), collapse = ", "), ")", 
-                         ", sigma=(", paste(round(sigma_truth_dd, 1), collapse = ", "), ")"), 
+       subtitle = paste0("N=", N, ", mu=(", paste(mu_d, collapse = ", "), ")", 
+                         ", sigma=(", paste(sqrt(solve(lambda_truth_dd)), collapse = ", "), ")"), 
        x = expression(x[1]), y = expression(x[2]), 
-       color = "density") # ラベル
+       color = "density")
 
 
-# 事後分布のパラメータを計算
-W_hat_dd <- solve(
-  (t(x_nd) - mu_d) %*% t(t(x_nd) - mu_d) + solve(W_dd)
+### 事前分布(ウィシャート分布)の設定 -----
+
+# lambdaの事前分布のパラメータを指定
+w_dd <- matrix(c(0.00005, 0, 0, 0.00005), nrow = 2, ncol = 2)
+nu <- 2
+
+# lambdaの期待値を計算:式(2.89)
+E_lambda_dd <- nu * w_dd
+
+# 事前分布の期待値を用いた分布を計算:式(2.72)
+prior_df <- tibble(
+  x_1 = x_point_mat[, 1], 
+  x_2 = x_point_mat[, 2], 
+  density = mvnfast::dmvn(
+    X = x_point_mat, mu = mu_d, sigma = solve(E_lambda_dd)
+  )
+)
+
+# 事前分布の期待値を用いた分布を作図
+ggplot() + 
+  geom_contour(data = prior_df, aes(x = x_1, y = x_2, z = density, color = ..level..)) + # lambdaの期待値を用いた分布
+  geom_contour(data = model_df, aes(x = x_1, y = x_2, z = density, color = ..level..), 
+               alpha = 0.5, linetype = "dashed") + # 尤度
+  labs(title = "Multivariate Gaussian Distribution", 
+       subtitle = paste0("nu=", nu, 
+                         ", W=(", paste(round(w_dd, 5), collapse = ", "), ")"), 
+       x = expression(x[1]), y = expression(x[2]), 
+       color = "density")
+
+
+### 事後分布(ウィシャート分布)の設定 -----
+
+# lambdaの事後分布のパラメータを計算:式(3.116)
+w_hat_dd <- solve(
+  (t(x_nd) - mu_d) %*% t(t(x_nd) - mu_d) + solve(w_dd)
 )
 nu_hat <- N + nu
 
-# 精度パラメータの期待値を計算
-lambda_E_dd <- nu_hat * W_hat_dd
+# lambdaの期待値を計算:式(2.89)
+E_lambda_hat_dd <- nu_hat * w_hat_dd
 
-# 事後分布の期待値を用いた分布を計算
+# 事後分布の期待値を用いた分布を計算:式(2.72)
 posterior_df <- tibble(
-  xy = point_df, 
-  density = mvtnorm::dmvnorm(x = xy, mean = mu_d, sigma = solve(lambda_E_dd)) # 確率密度
-) %>% 
-  dplyr::select(density) %>% 
-  cbind(point_df, .)
-
-# 作図
-ggplot() + 
-  geom_contour(data = posterior_df, aes(x, y, z = density, color = ..level..)) + # 精度の期待値を用いた分布
-  geom_contour(data = model_df, aes(x, y, z = density, color = ..level..), 
-               alpha = 0.5, linetype = "dashed") + # 観測モデル
-  geom_point(data = mu_df, aes(x = x, y = y), color = "red", shape = 3, size = 5) + # 平均値
-  labs(title = "Multivariate Gaussian Distribution", 
-       subtitle = paste0("N=", N, ", mu=(", paste(round(mu_d, 1), collapse = ", "), ")", 
-                         ", E_sigma_hat=(", paste(round(sqrt(solve(lambda_E_dd)), 1), collapse = ", "), ")"), 
-       x = expression(x[1]), y = expression(x[2]), 
-       color = "density") # ラベル
-
-
-# 予測分布 --------------------------------------------------------------------
-
-# 予測分布のパラメータを計算
-mu_s_d <- mu_d
-lambda_s_hat_dd <- (1 - D + nu_hat) * W_hat_dd
-nu_s_hat <- 1 - D + nu_hat
-
-# 予測分布を計算
-predict_df <- tibble(
-  xy = point_df, 
-  density = mvtnorm::dmvt(x = xy, delta = mu_s_d, sigma = solve(lambda_s_hat_dd), df = nu_s_hat) # 確率密度
-) %>% 
-  dplyr::select(density) %>% 
-  cbind(point_df, .)
-predict_df <- cbind(
-  point_df, 
-  density = mvnfast::dmvt(X = as.matrix(point_df), mu = mu_s_d, sigma = solve(lambda_s_hat_dd), df = nu_s_hat) # 確率密度
+  x_1 = x_point_mat[, 1], 
+  x_2 = x_point_mat[, 2], 
+  density = mvnfast::dmvn(
+    X = x_point_mat, mu = mu_d, sigma = solve(E_lambda_hat_dd)
+  )
 )
 
-# 作図
+# 事後分布の期待値を用いた分布を作図
 ggplot() + 
-  geom_contour(data = predict_df, aes(x, y, z = density, color = ..level..)) + # 予測分布
-  geom_contour(data = model_df, aes(x, y, z = density, color = ..level..), 
-               alpha = 0.5, linetype = "dashed") + # 観測モデル
-  geom_point(data = mu_df, aes(x = x, y = y), color = "red", shape = 3, size = 5) + # 平均パラメータ
-  labs(title = "Multivariate Student's t Distribution", 
-       subtitle = paste0("N=", N, ", mu_s=(", paste(round(mu_s_d, 1), collapse = ", "), ")", 
-                         ", lambda_s_hat=(", paste(round(lambda_s_hat_dd, 1), collapse = ", "), ")", 
-                         ", df=", nu_s_hat), 
+  geom_contour(data = posterior_df, aes(x = x_1, y = x_2, z = density, color = ..level..)) + # lambdaの期待値を用いた分布
+  geom_contour(data = model_df, aes(x = x_1, y = x_2, z = density, color = ..level..), 
+               alpha = 0.5, linetype = "dashed") + # 尤度
+  labs(title = "Multivariate Gaussian Distribution", 
+       subtitle = paste0("N=", N, ", nu_hat=", nu_hat, 
+                         ", W_hat=(", paste(round(w_hat_dd, 5), collapse = ", "), ")"), 
        x = expression(x[1]), y = expression(x[2]), 
-       color = "density") # ラベル
+       color = "density")
 
 
-# gif ---------------------------------------------------------------------
+### 予測分布(多次元スチューデントのt分布)の計算 -----
 
-# 追加パッケージ
+# 次元数:(固定)
+D <- 2
+
+# 予測分布のパラメータを計算:式(3.124)
+mu_s_d <- mu_d
+lambda_s_hat_dd <- (1 - D + nu_hat) * w_hat_dd
+nu_s_hat <- 1 - D + nu_hat
+
+# 予測分布を計算:式(3.121)
+predict_df <- tibble(
+  x_1 = x_point_mat[, 1], 
+  x_2 = x_point_mat[, 2], 
+  density = mvnfast::dmvt(
+    X = x_point_mat, mu = mu_s_d, sigma = solve(lambda_s_hat_dd), df = nu_s_hat
+  )
+)
+
+# 予測分布を作図
+ggplot() + 
+  geom_contour(data = predict_df, aes(x = x_1, y = x_2, z = density, color = ..level..)) + # 予測分布
+  geom_contour(data = model_df, aes(x = x_1, y = x_2, z = density, color = ..level..), 
+               alpha = 0.5, linetype = "dashed") + # 尤度
+  labs(title = "Multivariate Student's t Distribution", 
+       subtitle = paste0("N=", N, ", nu_s_hat=", nu_s_hat, 
+                         ", lambda_s_hat=(", paste(round(lambda_s_hat_dd, 5), collapse = ", "), ")"), 
+       x = expression(x[1]), y = expression(x[2]), 
+       color = "density")
+
+
+# ・アニメーション ---------------------------------------------------------------------
+
+# 利用するパッケージ
+library(tidyverse)
+library(mvnfast)
 library(gganimate)
 
 
-# データ数を指定
-N <- 100
-D <- 2 # (固定)
+### 推論処理 -----
 
-# 観測モデルのパラメータを指定
+# 真のパラメータを指定
 mu_d <- c(25, 50)
-sigma_truth_dd <- matrix(c(50, 30, 30, 50), nrow = 2, ncol = 2)
+sigma_truth_dd <- matrix(c(20, 15, 15, 30), nrow = 2, ncol = 2)
 lambda_truth_dd <- solve(sigma_truth_dd^2)
 
 # 事前分布のパラメータを指定
 nu <- 2
-W_dd <- matrix(c(0.00005, 0, 0, 0.00005), nrow = 2, ncol = 2)
+w_dd <- matrix(c(0.00005, 0, 0, 0.00005), nrow = 2, ncol = 2)
 
-# 精度パラメータの期待値を計算
-lambda_E_dd <- nu * W_dd
+# 初期値によるlambdaの期待値を計算:式(2.89)
+E_lambda_dd <- nu * w_dd
 
-
-# 作図用の点を生成
-x_vec <- seq(mu_d[1] - 3 * sigma_truth_dd[1, 1], mu_d[1] + 3 * sigma_truth_dd[1, 1], by = 1)
-y_vec <- seq(mu_d[2] - 3 * sigma_truth_dd[2, 2], mu_d[2] + 3 * sigma_truth_dd[2, 2], by = 1)
-point_df <- tibble(
-  x = rep(x_vec, times = length(y_vec)), 
-  y = rep(y_vec, each = length(x_vec))
-)
-mu_df <- tibble(
-  x = mu_d[1], 
-  y = mu_d[2]
-)
-
-# 観測モデルを計算
-model_df <- tibble(
-  xy = point_df, 
-  density = mvtnorm::dmvnorm(x = xy, mean = mu_d, sigma = sigma_truth_dd^2), # 確率密度
-) %>% 
-  dplyr::select(density) %>% 
-  cbind(point_df, .)
-
-
-# 事前分布の期待値を用いた分布を計算
-posterior_df <- tidyr::tibble(
-  xy = point_df, 
-  density = mvtnorm::dmvnorm(x = xy, mean = mu_d, sigma = solve(lambda_E_dd)), # 確率密度
-  iteration = 0 # 試行回数
-) %>% 
-  dplyr::select(density, iteration) %>% 
-  cbind(point_df, .)
-
-
-# 予測分布のパラメータを計算
+# 初期値による予測分布のパラメータを計算:式(3.124)
 mu_s_d <- mu_d
-lambda_s_dd <- (1 - D + nu) * W_dd
-nu_s <- 1 - D + nu
+lambda_s_dd <- (nu - 1) * w_dd
+nu_s <- nu - 1
 
-# 初期値による予測分布を計算
-predict_df <- cbind(
-  point_df, 
-  density = mvnfast::dmvt(X = as.matrix(point_df), mu = mu_s_d, sigma = solve(lambda_s_dd), df = nu_s), # 確率密度
-  iteration = 0 # 試行回数
+
+# 作図用のxの点を作成
+x_1_vec <- seq(mu_d[1] - 4 * sigma_truth_dd[1, 1], mu_d[1] + 4 * sigma_truth_dd[1, 1], by = 0.25)
+x_2_vec <- seq(mu_d[2] - 4 * sigma_truth_dd[2, 2], mu_d[2] + 4 * sigma_truth_dd[2, 2], by = 0.25)
+x_point_mat <- cbind(
+  rep(x_1_vec, times = length(x_2_vec)), 
+  rep(x_2_vec, each = length(x_1_vec))
 )
+
+# 事前分布の期待値を用いた分布を計算:式(2.72)
+posterior_df <- tibble(
+  x_1 = x_point_mat[, 1], 
+  x_2 = x_point_mat[, 2], 
+  density = mvnfast::dmvn(
+    X = x_point_mat, mu = mu_d, sigma = solve(E_lambda_dd)
+  ), 
+  label = as.factor(
+    paste0(
+      "N=", 0, ", nu=", nu, 
+      ", W=(", paste(round(w_dd, 5), collapse = ", "), ")"
+    )
+  )
+)
+
+# 予測分布を計算:式(3.121)
+predict_df <- tibble(
+  x_1 = x_point_mat[, 1], 
+  x_2 = x_point_mat[, 2], 
+  density = mvnfast::dmvt(
+    X = x_point_mat, mu = mu_s_d, sigma = solve(lambda_s_dd), df = nu_s
+  ), 
+  label = as.factor(
+    paste0(
+      "N=", 0, ", nu_s=", nu_s, 
+      ", lambda_s=(", paste(round(lambda_s_dd, 5), collapse = ", "), ")"
+    )
+  )
+)
+
+
+# データ数(試行回数)を指定
+N <- 50
+
+# 観測データの受け皿を初期化
+x_nd <- matrix(0, nrow = N, ncol = 2)
 
 # ベイズ推論
-for(i in 1:N) {
+for(n in 1:N) {
   
-  # 2次元ガウス分布に従うデータを生成
-  x_nd <- mvtnorm::rmvnorm(n = 1, mean = mu_d, sigma = sigma_truth_dd^2)
+  # 多次元ガウス分布に従うデータを生成
+  x_nd[n, ] <- mvnfast::rmvn(n = 1, mu = mu_d, sigma = solve(lambda_truth_dd)) %>% 
+    as.vector()
   
-  # 観測データを記録
-  if(i > 1) { # 初回以外
-    # オブジェクトを結合
-    x_mat <- rbind(x_mat, x_nd)
-    sample_df <- tibble(
-      x = x_mat[, 1],
-      y = x_mat[, 2], 
-      iteration = i
-    ) %>% 
-      rbind(sample_df, .)
-  } else if(i == 1){ # 初回
-    # オブジェクトを作成
-    x_mat <- x_nd
-    sample_df <- tibble(
-      x = x_mat[, 1],
-      y = x_mat[, 2], 
-      iteration = i
-    )
-  }
-  
-  # 事後分布のパラメータを更新
-  W_dd <- solve(
-    (t(x_nd) - mu_d) %*% t(t(x_nd) - mu_d) + solve(W_dd)
+  # 事後分布のパラメータを更新:式(3.116)
+  w_dd <- solve(
+    (x_nd[n, ] - mu_d) %*% t(x_nd[n, ] - mu_d) + solve(w_dd)
   )
   nu <- 1 + nu
   
-  # 精度パラメータの期待値を計算
-  lambda_E_dd <- nu * W_dd
+  # lambdaの期待値を計算:式(2.89)
+  E_lambda_dd <- nu * w_dd
   
-  # 事後分布の期待値を用いた分布を計算
-  tmp_posterior_df <- tidyr::tibble(
-    xy = point_df, 
-    density = mvtnorm::dmvnorm(x = xy, mean = mu_d, sigma = solve(lambda_E_dd)), # 確率密度
-    iteration = i # 試行回数
-  ) %>% 
-    dplyr::select(density, iteration) %>% 
-    cbind(point_df, .)
+  # 事後分布の期待値を用いた分布を更新:式(2.72)
+  tmp_posterior_df <- tibble(
+    x_1 = x_point_mat[, 1], 
+    x_2 = x_point_mat[, 2], 
+    density = mvnfast::dmvn(
+      X = x_point_mat, mu = mu_d, sigma = solve(E_lambda_dd)
+    ), 
+    label = as.factor(
+      paste0(
+        "N=", n, ", nu_hat=", nu, 
+        ", W_hat=(", paste(round(w_dd, 5), collapse = ", "), ")"
+      )
+    )
+  )
   
-  # 予測分布のパラメータを更新
-  mu_s_d <- mu_d
-  lambda_s_dd <- (1 - D + nu) * W_dd
-  nu_s <- 1 - D + nu
+  # 予測分布のパラメータを更新:式(3.124)
+  #mu_s_d <- mu_d
+  lambda_s_dd <- (nu - 1) * w_dd
+  nu_s <- nu - 1
   
-  # 予測分布を計算
-  tmp_predict_df <- cbind(
-    point_df, 
-    density = mvnfast::dmvt(X = as.matrix(point_df), mu = mu_s_d, sigma = solve(lambda_s_dd), df = nu_s), # 確率密度
-    iteration = i # 試行回数
+  # 予測分布を計算:式(3.121)
+  tmp_predict_df <- tibble(
+    x_1 = x_point_mat[, 1], 
+    x_2 = x_point_mat[, 2], 
+    density = mvnfast::dmvt(
+      X = x_point_mat, mu = mu_s_d, sigma = solve(lambda_s_dd), df = nu_s
+    ), 
+    label = as.factor(
+      paste0(
+        "N=", n, ", nu_s_hat=", nu_s, 
+        ", lambda_s_hat=(", paste(round(lambda_s_dd, 5), collapse = ", "), ")"
+      )
+    )
   )
   
   # 推論結果を結合
@@ -256,41 +291,84 @@ for(i in 1:N) {
   predict_df <- rbind(predict_df, tmp_predict_df)
   
   # 動作確認
-  print(i)
+  print(paste0("n=", n, " (", round(n / N * 100, 1), "%)"))
 }
 
+# 観測データを確認
+summary(x_nd)
+
+
+### 作図処理 -----
+
+# 尤度を計算:式(2.72)
+model_df <- tibble(
+  x_1 = x_point_mat[, 1], 
+  x_2 = x_point_mat[, 2], 
+  density = mvnfast::dmvn(
+    X = x_point_mat, mu = mu_d, sigma = solve(lambda_truth_dd)
+  )
+)
+
+
+# 作図用に観測データのデータフレームを作成
+label_vec <- unique(posterior_df[["label"]]) # lambdaの期待値を用いた分布のラベルを抽出
+x_df <- tibble(x_n1 = NA, x_n2 = NA, label = label_vec[1]) # 初期値用
+for(n in 1:N) {
+  # n個目までのデータフレームを作成
+  tmp_x_df <- tibble(
+    x_n1 = x_nd[1:n, 1], 
+    x_n2 = x_nd[1:n, 2], 
+    label = label_vec[n + 1]
+  )
+  
+  # 結合
+  x_df <- rbind(x_df, tmp_x_df)
+}
 
 # 事後分布の期待値を用いた分布を作図
 posterior_graph <- ggplot() + 
-  geom_contour(data = posterior_df, aes(x, y, z = density, color = ..level..)) + # 精度の期待値を用いた分布
-  geom_point(data = sample_df, aes(x = x, y = y)) + # 観測データ
-  geom_contour(data = model_df, aes(x, y, z = density, color = ..level..), 
-               alpha = 0.5, linetype = "dashed") + # 観測モデル
-  geom_point(data = mu_df, aes(x = x, y = y), color = "red", shape = 3, size = 5) + # 平均パラメータ
-  transition_manual(iteration) +  # フレーム
+  geom_contour(data = posterior_df, aes(x = x_1, y = x_2, z = density, color = ..level..)) + # lambdaの期待値を用いた分布
+  geom_contour(data = model_df, aes(x = x_1, y = x_2, z = density, color = ..level..), 
+               alpha = 0.5, linetype = "dashed") + # 尤度
+  geom_point(data = x_df, aes(x = x_n1, y = x_n2)) + # 観測データ
+  transition_manual(label) + # フレーム
   labs(title = "Multivariate Gaussian Distribution", 
-       subtitle = "N={current_frame}", 
+       subtitle = "{current_frame}", 
        x = expression(x[1]), y = expression(x[2]), 
-       color = "density") # ラベル
+       color = "density")
 
 # gif画像を作成
-animate(posterior_graph, nframes = N + 1, fps = 5)
+animate(posterior_graph, nframes = N + 1, fps = 10)
 
+
+# 作図用に観測データのデータフレームを作成
+label_vec <- unique(predict_df[["label"]]) # 予測分布のラベルを抽出
+x_df <- tibble(x_n1 = NA, x_n2 = NA, label = label_vec[1]) # 初期値用
+for(n in 1:N) {
+  # n個目までのデータフレームを作成
+  tmp_x_df <- tibble(
+    x_n1 = x_nd[1:n, 1], 
+    x_n2 = x_nd[1:n, 2], 
+    label = label_vec[n + 1]
+  )
+  
+  # 結合
+  x_df <- rbind(x_df, tmp_x_df)
+}
 
 # 予測分布を作図
 predict_graph <- ggplot() + 
-  geom_contour(data = predict_df, aes(x, y, z = density, color = ..level..)) + # 予測分布
-  geom_point(data = sample_df, aes(x = x, y = y)) + # 観測データ
-  geom_contour(data = model_df, aes(x, y, z = density, color = ..level..), 
-               alpha = 0.5, linetype = "dashed") + # 観測モデル
-  geom_point(data = mu_df, aes(x = x, y = y), color = "red", shape = 3, size = 5) +  # 平均パラメータ
-  transition_manual(iteration) +  # フレーム
+  geom_contour(data = predict_df, aes(x = x_1, y = x_2, z = density, color = ..level..)) + # 予測分布
+  geom_contour(data = model_df, aes(x = x_1, y = x_2, z = density, color = ..level..), 
+               alpha = 0.5, linetype = "dashed") + # 尤度
+  geom_point(data = x_df, aes(x = x_n1, y = x_n2)) + # 観測データ
+  transition_manual(label) + # フレーム
   labs(title = "Multivariate Student's t Distribution", 
-       subtitle = "N={current_frame}", 
+       subtitle = "{current_frame}", 
        x = expression(x[1]), y = expression(x[2]), 
-       color = "density") # ラベル
+       color = "density")
 
 # gif画像を作成
-animate(predict_graph, nframes = N + 1, fps = 5)
+animate(predict_graph, nframes = N + 1, fps = 10)
 
 
