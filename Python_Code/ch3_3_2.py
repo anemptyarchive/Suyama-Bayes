026f9191e50a1c0009d218ad2868e1d0efc1b399
Shoko <61862770+anemptyarchive@users.noreply.github.com>
2021-03-17 11:43:50 +0900

    upload ch3.3.3 Python fig

--- Python_Code/ch3_3_2.py
+++ Python_Code/ch3_3_2.py
@@ -5,7 +5,7 @@
 # 3.3.2項で利用するライブラリ
 import numpy as np
 import math # 対数ガンマ関数:lgamma()
-from scipy.stats import norm, gamma, t # ガウス分布, ガンマ分布, スチューデントのt分布
+from scipy.stats import norm, gamma, t # 1次元ガウス分布, ガンマ分布, 1次元スチューデントのt分布
 import matplotlib.pyplot as plt
 
 #%%
@@ -15,30 +15,31 @@ import matplotlib.pyplot as plt
 # 真のパラメータを指定
 mu = 25
 lambda_truth = 0.01
+print(np.sqrt(1 / lambda_truth)) # 標準偏差
 
 # 作図用のxの値を設定
-x_line = np.arange(
+x_line = np.linspace(
     mu - 4 * np.sqrt(1 / lambda_truth), 
     mu + 4 * np.sqrt(1 / lambda_truth), 
-    0.1
+    num=1000
 )
 
 # 尤度を計算:式(2.64)
-C_N = 1 / np.sqrt(2 * np.pi / lambda_truth) # 正規化項
-true_model = C_N * np.exp(- 0.5 * lambda_truth * (x_line - mu)**2) # 確率密度
+ln_C_N = - 0.5 * (np.log(2 * np.pi) - np.log(lambda_truth)) # 正規化項(対数)
+true_model = np.exp(ln_C_N - 0.5 * lambda_truth * (x_line - mu)**2) # 確率密度
 
 # 尤度を計算:SciPy ver
-true_model = norm.pdf(x=x_line, loc=mu, scale=np.sqrt(1 / lambda_truth)) # 確率密度
+#true_model = norm.pdf(x=x_line, loc=mu, scale=np.sqrt(1 / lambda_truth)) # 確率密度
 
 #%%
 
 # 尤度を作図
-plt.plot(x_line, true_model, label='true model', color='purple') # 尤度
+plt.figure(figsize=(12, 9))
+plt.plot(x_line, true_model, color='purple') # 尤度
 plt.xlabel('x')
 plt.ylabel('density')
 plt.suptitle('Gaussian Distribution', fontsize=20)
 plt.title('$\mu=' + str(mu) + ', \lambda=' + str(lambda_truth) + '$', loc='left')
-plt.legend()
 plt.show()
 
 #%%
@@ -55,12 +56,13 @@ print(x_n[:5])
 #%%
 
 # 観測データのヒストグラムを作図
+plt.figure(figsize=(12, 9))
 plt.hist(x=x_n, bins=50) # 観測データ
 plt.xlabel('x')
 plt.ylabel('count')
-plt.suptitle('Observation Data', fontsize=20)
+plt.suptitle('Gaussian Distribution', fontsize=20)
 plt.title('$N=' + str(N) + ', \mu=' + str(mu) + 
-          ', \lambda=' + str(lambda_truth) + '$', loc='left')
+          ', \sigma=' + str(np.sqrt(1 / lambda_truth)) + '$', loc='left')
 plt.show()
 
 #%%
@@ -72,24 +74,24 @@ a = 1
 b = 1
 
 # 作図用のlambdaの値を設定
-lambda_line = np.arange(0, 4 * lambda_truth, 0.00001)
+lambda_line = np.linspace(0, 4 * lambda_truth, num=1000)
 
 # lambdaの事前分布を計算:式(2.56)
 ln_C_Gam = a * np.log(b) - math.lgamma(a) # 正規化項(対数)
 prior = np.exp(ln_C_Gam + (a - 1) * np.log(lambda_line) - b * lambda_line) # 確率密度
 
 # lambdaの事前分布を計算:SciPy ver
-prior = gamma.pdf(x=lambda_line, a=a, scale=1 / b) # 確率密度
+#prior = gamma.pdf(x=lambda_line, a=a, scale=1 / b) # 確率密度
 
 #%%
 
 # lambdaの事前分布を作図
+plt.figure(figsize=(12, 9))
 plt.plot(lambda_line, prior, label='prior', color='purple') # lambdaの事前分布
 plt.xlabel('$\lambda$')
 plt.ylabel('density')
 plt.suptitle('Gamma Distribution', fontsize=20)
 plt.title('$a=' + str(a) + ', b=' + str(b) + '$', loc='left')
-plt.legend()
 plt.show()
 
 #%%
@@ -111,7 +113,8 @@ posterior = np.exp(ln_C_Gam + (a_hat - 1) * np.log(lambda_line) - b_hat * lambda
 
 #%%
 
-# lambdaの事前分布を作図
+# lambdaの事後分布を作図
+plt.figure(figsize=(12, 9))
 plt.plot(lambda_line, posterior, label='posterior', color='purple') # lambdaの事後分布
 plt.vlines(x=lambda_truth, ymin=0, ymax=max(posterior), label='$\lambda_{truth}$', 
            color='red', linestyle='--') # 真のlambda
@@ -127,28 +130,34 @@ plt.show()
 ## 予測分布(スチューデントのt分布)の計算
 
 # 予測分布のパラメータを計算:式(3.79')
-mu_s_hat = mu
+mu_s = mu
 lambda_s_hat = a_hat / b_hat
 nu_s_hat = 2 * a_hat
+#lambda_s_hat = (N + 2 * a) / (np.sum((x_n - mu)**2) + 2 * b)
+#nu_s_hat = N + 2 * a
+print(mu_s)
+print(lambda_s_hat)
+print(nu_s_hat)
 
 # 予測分布を計算:式(3.76)
 ln_C_St = math.lgamma(0.5 * (nu_s_hat + 1)) - math.lgamma(0.5 * nu_s_hat) # 正規化項(対数)
 ln_term1 = 0.5 * np.log(lambda_s_hat / np.pi / nu_s_hat)
-ln_term2 = - 0.5 * (nu_s_hat + 1) * np.log(1 + lambda_s_hat / nu_s_hat * (x_line - mu_s_hat)**2)
+ln_term2 = - 0.5 * (nu_s_hat + 1) * np.log(1 + lambda_s_hat / nu_s_hat * (x_line - mu_s)**2)
 predict = np.exp(ln_C_St + ln_term1 + ln_term2) # 確率密度
 
 # 予測分布を計算:SciPy ver
-predict = t.pdf(x=x_line, df=nu_s_hat, loc=mu_s_hat, scale=np.sqrt(1 / lambda_s_hat))
+#predict = t.pdf(x=x_line, df=nu_s_hat, loc=mu_s, scale=np.sqrt(1 / lambda_s_hat))
 
 #%%
 
 # 予測分布を作図
+plt.figure(figsize=(12, 9))
 plt.plot(x_line, predict, label='predict', color='purple') # 予測分布
 plt.plot(x_line, true_model, label='true', color='red', linestyle='--') # 真の分布
 plt.xlabel('x')
 plt.ylabel('density')
 plt.suptitle("Student's t Distribution", fontsize=20)
-plt.title('$N=' + str(N) + ', \hat{\mu}_s=' + str(mu_s_hat) + 
+plt.title('$N=' + str(N) + ', \mu_s=' + str(mu_s) + 
           ', \hat{\lambda}_s=' + str(np.round(lambda_s_hat, 3)) + 
           ', \hat{\\nu}_s=' + str(nu_s_hat) + '$', loc='left')
 plt.legend()
@@ -160,7 +169,7 @@ plt.show()
 
 # 利用するライブラリ
 import numpy as np
-from scipy.stats import norm, gamma, t # ガウス分布, ガンマ分布, スチューデントのt分布
+from scipy.stats import norm, gamma, t # 1次元ガウス分布, ガンマ分布, 1次元スチューデントのt分布
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 
@@ -181,16 +190,18 @@ mu_s = mu
 lambda_s = a / b
 nu_s = 2 * a
 
-# データ数(試行回数)を指定
-N = 100
-
-# 作図用の値を設定
-x_line = np.arange(
+# 作図用のxの値を設定
+x_line = np.linspace(
     mu - 4 * np.sqrt(1 / lambda_truth), 
     mu + 4 * np.sqrt(1 / lambda_truth), 
-    0.1
+    num=1000
 )
-lambda_line = np.arange(0, 4 * lambda_truth, 0.00001)
+
+# 作図用のlambdaの値を設定
+lambda_line = np.linspace(0, 4 * lambda_truth, num=1000)
+
+# データ数(試行回数)を指定
+N = 100
 
 # 推移の記録用の受け皿を初期化
 x_n = np.empty(N)
