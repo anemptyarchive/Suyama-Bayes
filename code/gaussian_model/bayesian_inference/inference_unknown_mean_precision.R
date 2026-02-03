4d0ecdb4a265b55bb16920a5ec0eae471b76fd76
Shoko <61862770+anemptyarchive@users.noreply.github.com>
2026-02-03 22:24:01 +0900

    全体をアプデ

--- code/gaussian_model/bayesian_inference/inference_unknown_mean_precision.R
+++ code/gaussian_model/bayesian_inference/inference_unknown_mean_precision.R
@@ -1,79 +1,65 @@
 
-# 3.3.3 1次元ガウス分布の学習と予測：平均・精度が未知の場合 --------------------------------------------------------------
+# 1次元ガウスモデル ------------------------------------------------------------
+
+# chapter 3.3.3
+# 平均と精度が未知の場合
+# ベイズ推論
+# 推論アルゴリズムの実装
+
+
+# パッケージの読込 -------------------------------------------------------------
 
 # 利用パッケージ
 library(tidyverse)
-library(gganimate)
 library(LaplacesDemon)
 
-# チェック用
+# パッケージ名の省略用
 library(ggplot2)
 
 
 # ベイズ推論の実装 ----------------------------------------------------------------
 
-### ・生成分布(ガウス分布)の設定 -----
+### 生成分布(ガウス分布)の設定 -----
 
-# 真の平均パラメータを指定
-mu_truth <- 25
+#### パラメータの設定 -----
 
-# 真の精度パラメータを指定
-lambda_truth <- 0.01
-sqrt(1 / lambda_truth) # 標準偏差
+# 真のパラメータを指定
+mu_truth     <- 5
+lambda_truth <- 0.25
+1/sqrt(lambda_truth) # 標準偏差
 
 
-# グラフ用のxの値を作成
-x_vec <- seq(
-  mu_truth - 1/sqrt(lambda_truth) * 4, 
-  mu_truth + 1/sqrt(lambda_truth) * 4, 
-  length.out = 201
-)
+#### 変数の設定 -----
 
-# 真の分布を計算:式(2.64)
+# x軸の範囲を設定
+k <- 4
+u <- 5
+x_size <- (1/sqrt(lambda_truth)) |> #標準偏差
+  (\(.) {. * k})() |> # 定数倍
+  #(\(.) {max(., abs(x_n-mu_truth))})() |> # # サンプルと比較
+  (\(.) {ceiling(. /u)*u})() # u単位で切り上げ
+x_min <- mu_truth - x_size
+x_max <- mu_truth + x_size
+
+# x軸の値を作成
+x_vec <- seq(from = x_min, to = x_max, length.out = 1001)
+
+
+#### 分布の計算 -----
+
+# 生成分布の確率密度を計算
 model_df <- tibble::tibble(
-  x = x_vec, # 確率変数
-  dens = dnorm(x = x_vec, mean = mu_truth, sd = 1/sqrt(lambda_truth)) # 確率密度
+  x      = x_vec, # 確率変数
+  mu     = mu_truth,     # 平均パラメータ
+  lambda = lambda_truth, # 精度パラメータ
+  sigma  = 1/sqrt(lambda), # 標準偏差パラメータ
+  dens   = dnorm(x = x, mean = mu, sd = sigma) # 確率密度
 )
 
-# 真の分布を作図
-ggplot() + 
-  geom_line(data = model_df, mapping = aes(x = x, y = dens, color = "model"), 
-            size = 1) + # 真の分布
-  scale_color_manual(breaks = "model", values = "purple", labels = "true model", name = "") + # 線の色:(凡例表示用)
-  labs(title = "Gaussian Distribution", 
-       subtitle = parse(text = paste0("list(mu==", mu_truth, ", lambda==", lambda_truth, ")")), 
-       x = "x", y = "density")
 
+### 事前分布(ガウス分布)の設定 -----
 
-### ・データの生成 -----
-
-# (観測)データ数を指定
-N <- 50
-
-
-# ガウス分布に従うデータを生成
-x_n <- rnorm(n = N, mean = mu_truth, sd = 1/sqrt(lambda_truth))
-
-# 観測データを格納
-data_df <- tibble::tibble(x = x_n)
-
-# 観測データのヒストグラムを作成
-ggplot() + 
-  geom_histogram(data = data_df, mapping = aes(x = x, y = ..density.., fill = "data"), 
-                 bins = 30) + # 観測データ:(密度)
-  geom_line(data = model_df, mapping = aes(x = x, y = dens, color = "model"), 
-            size = 1, linetype = "dashed") + # 真の分布
-  scale_fill_manual(values = c(model = NA, data = "pink"), na.value = NA, 
-                    labels = c(model = "true model", data = "observation data"), name = "") + # バーの色:(凡例表示用)
-  scale_color_manual(values = c(model = "red", data = "pink"), 
-                     labels = c(model = "true model", data = "observation data"), name = "") + # 線の色:(凡例表示用)
-  guides(color = guide_legend(override.aes = list(size = c(0.5, 0.5), linetype = c("dashed", "blank")))) + # 凡例の体裁:(凡例表示用)
-  labs(title = "Gaussian Distribution", 
-       subtitle = parse(text = paste0("list(mu==", mu_truth, ", lambda==", lambda_truth, ", N==", N, ")")), 
-       x = "x", y = "density")
-
-
-### ・事前分布(ガウス・ガンマ分布)の設定 -----
+#### パラメータの設定 -----
 
 # μの事前分布のパラメータを指定
 m    <- 0
@@ -84,52 +70,94 @@ a <- 1
 b <- 1
 
 
-# グラフ用のμの値を作成
-mu_vec <- seq(mu_truth - 40, mu_truth + 40, length.out = 201)
+#### 変数の設定 -----
 
-# グラフ用のλの値を作成
-lambda_vec <- seq(0, lambda_truth * 4, length.out = 201)
+# μ軸の範囲を設定
+mu_min = x_min
+mu_max = x_max
 
-# グラフ用のμとλの点を作成
-mu_lambda_mat <- tidyr::expand_grid(mu = mu_vec, lambda = lambda_vec) |> # 格子点を作成
-  as.matrix() # マトリクスに変換
-
-# μとλの(同時)事前分布を計算
-prior_df <- tidyr::tibble(
-  mu = mu_lambda_mat[, 1], # 確率変数μ
-  lambda = mu_lambda_mat[, 2], # 確率変数λ
-  N_dens = dnorm(x = mu, mean = m, sd = 1/sqrt(beta*lambda)), # μの確率密度
-  Gam_dens = dgamma(x = lambda, shape = a, rate = b), # λの確率密度
-  density = N_dens * Gam_dens # 確率密度
-)
-
-# μとλの(同時)事前分布を作図:等高線図
-ggplot() + 
-  geom_contour(data = prior_df, aes(x = mu, y = lambda, z = density, color = ..level.., alpha = "prior")) + # μとλの事前分布
-  geom_point(mapping = aes(x = mu_truth, y = lambda_truth, alpha = "data"), 
-             color = "red", size = 6, shape = 4) + # 真のパラメータ
-  scale_alpha_manual(values = c(param = 1, prior = 1), 
-                     labels = c(param = "true parameter", prior = "prior"), name = "") + # (凡例表示用の黒魔術)
-  guides(alpha = guide_legend(override.aes = list(shape = c(4, NA), linetype = c("blank", "solid")))) + # (凡例表示用の黒魔術)
-  labs(title = "Gaussian-Gamma Distribution", 
-       subtitle = parse(text = paste0("list(m==", m, ", beta==", beta, ", a==", a, ", b==", b, ")")), 
-       color = "density", 
-       x = expression(mu), y = expression(lambda))
-
-# μとλの(同時)事前分布を作図:塗りつぶし等高線図
-ggplot() + 
-  geom_contour_filled(data = prior_df, aes(x = mu, y = lambda, z = density, fill = ..level..), 
-                      alpha = 0.8, size = 0) + # μとλの事前分布
-  geom_point(mapping = aes(x = mu_truth, y = lambda_truth, color = "param"), 
-             size = 6, shape = 4) + # 真のパラメータ
-  scale_color_manual(breaks = "param", values = "red", labels = "true parameter", name = "") + # (凡例表示用の黒魔術)
-  labs(title = "Gaussian-Gamma Distribution", 
-       subtitle = parse(text = paste0("list(m==", m, ", beta==", beta, ", a==", a, ", b==", b, ")")), 
-       fill = "density", 
-       x = expression(mu), y = expression(lambda))
+# μ軸の値を作成
+mu_vec <- seq(from = mu_min, to = mu_max, length.out = 101)
 
 
-### ・事後分布(ガウス・ガンマ分布)の計算 -----
+# λ軸の範囲を設定
+k <- 3
+u <- 0.5
+lambda_min <- 0
+lambda_max <- lambda_truth |> # 真のパラメータ
+  (\(.) {. * k})() |> # 定数倍
+  (\(.) {ceiling(. /u)*u})() # u単位で切り上げ
+
+# λ軸の値を作成
+lambda_vec <- seq(from = lambda_min, to = lambda_max, length.out = 101)
+
+
+#### 分布の計算 -----
+
+# 事前分布の確率密度を計算
+prior_df <- tidyr::expand_grid(
+  mu     = mu_vec,    # μの確率変数
+  lambda = lambda_vec # λの確率変数
+) |> # 格子点を作成
+  dplyr::mutate(
+    m         = m,                 # 平均パラメータ
+    beta      = beta,              # 係数パラメータ
+    lambda_mu = beta * lambda,     # 精度パラメータ
+    sigma_mu  = 1/sqrt(lambda_mu), # 標準偏差パラメータ
+    a         = a,                 # 形状パラメータ
+    b         = b,                 # 尺度パラメータ
+    N_dens    = dnorm(x = mu, mean = m, sd = sigma_mu),  # μの確率密度
+    Gam_dens  = dgamma(x = lambda, shape = a, rate = b), # λの確率密度
+    dens      = N_dens * Gam_dens # μ, λの確率密度
+  )
+
+
+### 事前分布(ガウス分布)の設定 -----
+
+#### パラメータの設定 -----
+
+# データ数を指定
+N <- 50
+
+# 観測データを生成
+x_n <- rnorm(n = N, mean = mu_truth, sd = 1/sqrt(lambda_truth))
+
+
+#### データの集計 -----
+
+# 階級数を指定
+bin_num <- 40
+
+# 階級幅を計算
+bin_size <- (x_max - x_min) / bin_num
+
+# 境界値の範囲を設定
+bin_min <- x_min - 0.5*bin_size
+bin_max <- x_max + 0.5*bin_size
+
+# 観測データを集計
+obs_df <- tibble::tibble(
+  x = x_n # サンプル値
+) |> 
+  dplyr::mutate(
+    bin_i  = (x - bin_min) %/% bin_size,        # 階級番号
+    center = bin_min + (bin_i + 0.5) * bin_size # 階級値
+  ) |> 
+  dplyr::count(
+    center, name = "freq" # 度数
+  ) |> 
+  dplyr::mutate(
+    dens = freq / (bin_size * N) # 密度
+  ) |> 
+  tidyr::complete(
+    center = seq(from = x_min, to = x_max, by = bin_size), 
+    fill = list(freq = 0, dens = 0)
+  ) # 未観測値を補完
+
+
+### 事後分布(ガウス・ガンマ分布)の計算 -----
+
+#### パラメータの計算 -----
 
 # μの事後分布のパラメータを計算:式(3.83)
 beta_hat <- N + beta
@@ -140,82 +168,190 @@ a_hat <- 0.5 * N + a
 b_hat <- 0.5 * (sum(x_n^2) + beta * m^2 - beta_hat * m_hat^2) + b
 
 
-# μとλの(同時)事後分布を計算
-posterior_df <- tidyr::tibble(
-  mu = mu_lambda_mat[, 1], # 確率変数μ
-  lambda = mu_lambda_mat[, 2], # 確率変数λ
-  N_dens = dnorm(x = mu, mean = m_hat, sd = 1/sqrt(beta_hat*lambda)), # μの確率密度
-  Gam_dens = dgamma(x = lambda, shape = a_hat, rate = b_hat), # λの確率密度
-  density = N_dens * Gam_dens # 確率密度
-)
+#### 分布の計算  -----
 
-# パラメータラベル用の文字列を作成
-param_text <- paste0(
-  "list(hat(m)==", round(m_hat, 2), ", hat(beta)==", beta_hat, 
-  ", hat(a)==", a_hat, ", hat(b)==", round(b_hat, 1), ")"
-)
+# 事後分布の確率密度を計算
+posterior_df <- tidyr::expand_grid(
+  mu     = mu_vec,    # μの確率変数
+  lambda = lambda_vec # λの確率変数
+) |> # 格子点を作成
+  dplyr::mutate(
+    m         = m_hat,             # 平均パラメータ
+    beta      = beta_hat,          # 係数パラメータ
+    lambda_mu = beta * lambda,     # 精度パラメータ
+    sigma_mu  = 1/sqrt(lambda_mu), # 標準偏差パラメータ
+    a         = a_hat,             # 形状パラメータ
+    b         = b_hat,             # 尺度パラメータ
+    N_dens    = dnorm(x = mu, mean = m, sd = sigma_mu),  # μの確率密度
+    Gam_dens  = dgamma(x = lambda, shape = a, rate = b), # λの確率密度
+    dens      = N_dens * Gam_dens # μ, λの確率密度
+  )
 
-# μとλの(同時)事後分布を作図:等高線図
+
+#### 分布の作図 -----
+
+# 事後分布のラベルを作成
+posterior_param_lbl <- paste0(
+  "list(", 
+  "N == ",             N, ", ", 
+  "mu[truth] == ",     round(mu_truth,     digits = 2), ", ", 
+  "lambda[truth] == ", round(lambda_truth, digits = 5), ", ", 
+  "hat(m) == ",        round(m_hat,        digits = 2), ", ", 
+  "hat(beta) == ",     round(beta_hat,     digits = 1),  ", ", 
+  "hat(a) == ",        round(a_hat,        digits = 1), ", ", 
+  "hat(b) == ",        round(b_hat,        digits = 1), 
+  ")"
+) |> 
+  parse(text = _)
+
+# 確率密度軸の範囲を設定
+u <- 0.5
+dens_max <- max(
+  prior_df |> 
+    dplyr::pull(dens) |> 
+    max(), 
+  posterior_df |> 
+    dplyr::pull(dens) |> 
+    max()
+) |> 
+  (\(.) {ceiling(. /u)*u})() # u単位で切り上げ
+
+# 等高線を設定
+level_num     <- 11
+dens_vals <- seq(from = 0, to = dens_max, length.out = level_num)
+dens_vals
+
+# 事後分布を作図
 ggplot() + 
-  geom_contour(data = posterior_df, aes(x = mu, y = lambda, z = density, color = ..level.., alpha = "posterior")) + # μとλの事後分布
-  geom_point(mapping = aes(x = mu_truth, y = lambda_truth, alpha = "param"), 
-             color = "red", size = 6, shape = 4) + # 真のパラメータ
-  scale_alpha_manual(values = c(param = 1, posterior = 1), 
-                     labels = c(param = "true parameter", posterior = "posterior"), name = "") + # (凡例表示用の黒魔術)
-  guides(alpha = guide_legend(override.aes = list(shape = c(4, NA), linetype = c("blank", "solid")))) + # (凡例表示用の黒魔術)
-  labs(title = "Gaussian-Gamma Distribution", 
-       subtitle = parse(text = param_text), 
-       color = "density", 
-       x = expression(mu), y = expression(lambda))
+  geom_contour(
+    data    = prior_df, 
+    mapping = aes(x = mu, y = lambda, z = dens, color = after_stat(level), linetype = "prior"), 
+    #breaks = dens_vals, # (軸の共通化用)
+    bins = level_num
+  ) + # 事前分布
+  geom_contour_filled(
+    data    = posterior_df, 
+    mapping = aes(x = mu, y = lambda, z = dens, fill = after_stat(level), linetype = "posterior"), 
+    breaks = dens_vals, # (軸の共通化用)
+    alpha = 0.5
+  ) + # 事後分布
+  geom_vline(
+    mapping = aes(xintercept = mu_truth, linetype = "model"), 
+    color = "red", linewidth = 1
+  ) + # 真のパラメータ
+  geom_hline(
+    mapping = aes(yintercept = lambda_truth, linetype = "model"), 
+    color = "red", linewidth = 1
+  ) + # 真のパラメータ
+  scale_x_continuous(
+    sec.axis = sec_axis(
+      transform = ~ ., 
+      breaks    = mu_truth, 
+      labels    = expression(mu[truth])
+    ) # パラメータラベル
+  ) + 
+  scale_y_continuous(
+    sec.axis = sec_axis(
+      transform = ~ ., 
+      breaks    = lambda_truth, 
+      labels    = expression(lambda[truth])
+    ) # パラメータラベル
+  ) + 
+  scale_color_viridis_c(
+    limits = c(0, dens_max) # (軸の共通化用)
+  ) + 
+  scale_linetype_manual(
+    breaks = c("model", "prior", "posterior"), 
+    values = c("dashed", "dotted", "solid"), 
+    labels = c("true parameter", "prior distribution", "posterior distribution"), 
+    name = ""
+  ) + # (凡例表示用)
+  guides(
+    linetype = guide_legend(override.aes = list(linewidth = 0.5), order = 1), 
+    color    = guide_colorbar(order = 2), 
+    fill     = guide_legend(order = 3)
+  ) + 
+  labs(
+    title = "Gaussian-Gamma distribution", 
+    subtitle = posterior_param_lbl, 
+    fill  = "density", 
+    color = "density", 
+    x = expression(mu), 
+    y = expression(lambda)
+  )
 
-# μとλの(同時)事後分布を作図:塗りつぶし等高線図
-ggplot() + 
-  geom_contour_filled(data = posterior_df, aes(x = mu, y = lambda, z = density, fill = ..level..), 
-                      alpha = 0.8) + # μとλの事後分布
-  geom_point(mapping = aes(x = mu_truth, y = lambda_truth, color = "param"), 
-             size = 6, shape = 4) + # 真のパラメータ
-  scale_color_manual(breaks = "param", values = "red", labels = "true parameter", name = "") + # (凡例表示用の黒魔術)
-  labs(title = "Gaussian-Gamma Distribution", 
-       subtitle = parse(text = param_text), 
-       fill = "density", 
-       x = expression(mu), y = expression(lambda))
 
+### 予測分布(スチューデントのt分布)を計算 -----
 
-### ・予測分布(スチューデントのt分布)の計算 -----
+#### パラメータの計算 -----
 
 # 予測分布のパラメータを計算:式(3.95')
-mu_st_hat     <- m_hat
-lambda_st_hat <- beta_hat * a_hat / (1 + beta_hat) / b_hat
-nu_st_hat     <- 2 * a_hat
-#mu_st_hat     <- (sum(x_n) + beta * m) / (N + beta)
-#numer_lambda  <- (N + beta) * (N / 2 + a)
-#denom_lambda  <- (N + 1 + beta) * ((sum(x_n^2) + beta * m^2 - beta_hat * m_hat^2) / 2 + b)
-#lambda_st_hat <- numer_lambda / denom_lambda
-#nu_st_hat     <- N + 2 * a
+mu_s_hat     <- m_hat
+lambda_s_hat <- beta_hat / (1 + beta_hat) * a_hat / b_hat
+nu_s_hat     <- 2 * a_hat
 
-# 予測分布を計算:式(3.76)
+mu_s_hat     <- (sum(x_n) + beta * m) / (N + beta)
+term_1       <- (N + beta) / (N + 1 + beta)
+term_2       <- (0.5 * N + a)
+term_3       <- 0.5 * (sum(x_n^2) + beta * m^2 - 1/(N + beta) * (sum(x_n) + beta * m)^2) + b
+lambda_s_hat <- term_1 * term_2 / term_3
+nu_s_hat     <- N + 2 * a
+
+
+#### 分布の計算 -----
+
+# 予測分布の確率密度を計算
 predict_df <- tibble::tibble(
-  x = x_vec, # 確率変数
-  dens = LaplacesDemon::dst(x = x_vec, mu = mu_st_hat, sigma = 1/sqrt(lambda_st_hat), nu = nu_st_hat) # 確率密度
+  x        = x_vec, # 確率変数
+  mu_s     = mu_s_hat,         # 位置パラメータ
+  lambda_s = lambda_s_hat,     # 逆尺度パラメータ
+  sigma_s  = 1/sqrt(lambda_s), # 尺度パラメータ
+  nu_s     = nu_s_hat,         # 自由度パラメータ
+  dens     = LaplacesDemon::dst(x = x, mu = mu_s, sigma = sigma_s, nu = nu_s) # 確率密度
 )
 
-# パラメータラベル用の文字列を作成
-param_text <- paste0(
-  "list(N==", N, ", hat(mu)[s]==", round(mu_st_hat, 2), 
-  ", hat(lambda)[s]==", round(lambda_st_hat, 5), ", hat(nu)[s]==", nu_st_hat, ")"
-)
+
+#### 分布の作図 -----
+
+# 予測分布のラベルを作成
+predict_param_lbl <- paste0(
+  "list(", 
+  "N == ",              N, ", ", 
+  "mu[truth] == ",      round(mu_truth,     digits = 2), ", ", 
+  "lambda[truth] == ",  round(lambda_truth, digits = 5), ", ", 
+  "hat(mu)[s] == ",     round(mu_s_hat,     digits = 2), ", ", 
+  "hat(lambda)[s] == ", round(lambda_s_hat, digits = 5), ", ", 
+  "hat(nu)[s] == ",     round(nu_s_hat,     digits = 1), 
+  ")"
+) |> 
+  parse(text = _)
 
 # 予測分布を作図
 ggplot() + 
-  geom_line(data = model_df, mapping = aes(x = x, y = dens, color = "model"), 
-            size = 1, linetype = "dashed") + # 真の分布
-  geom_line(data = predict_df, mapping = aes(x = x, y = dens, color = "predict"), 
-            size = 1) + # 予測分布
-  scale_color_manual(values = c(model = "red", predict = "purple"), 
-                     labels = c(model = "true model", predict = "predict"), name = "") + # 線の色:(凡例表示用)
-  guides(color = guide_legend(override.aes = list(size = c(0.5, 0.5), linetype = c("dashed", "solid")))) + # 凡例の体裁:(凡例表示用)
-  labs(title = "Student's t Distribution", 
-       subtitle = parse(text = param_text), 
-       x = "x", y = "density")
+  geom_line(
+    data    = model_df, 
+    mapping = aes(x = x, y = dens, color = "model"), 
+    linewidth = 1, linetype = "dashed"
+  ) + # 真の分布
+  geom_line(
+    data    = predict_df, 
+    mapping = aes(x = x, y = dens, color = "predict"), 
+    linewidth = 1
+  ) + # 予測分布
+  scale_color_manual(
+    breaks = c("model", "predict"), 
+    values = c("red", "purple"), 
+    labels = c("true model", "predict distribution"), 
+    name   = ""
+  ) + # (凡例表示用)
+  guides(
+    color = guide_legend(override.aes = list(linewidth = 0.5))
+  ) + 
+  labs(
+    title = "Student's t distribution", 
+    subtitle = predict_param_lbl, 
+    x = expression(x), 
+    y = "density"
+  )
 
 
